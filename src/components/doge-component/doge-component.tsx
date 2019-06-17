a188e8eb87b3cd8d3cf06f27728727f81f8d3ea4
Maria Jose Tamayo <maria@bestateless.com>
2019-06-17 01:31:44 -0600

    first commit

---
+++ src/components/doge-component/doge-component.tsx
@@ -0,0 +1,18 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+	tag: 'doge-component',
+	styleUrl: 'doge-component.css',
+	shadow: true,
+})
+export class MyComponent {
+	public render() {
+		return (
+			<img
+				class="doge-img"
+				src="https://ih0.redbubble.net/image.407032251.6292/ap,550x550,12x12,1,transparent,t.u2.png"
+			/>
+		);
+	}
+}
+
